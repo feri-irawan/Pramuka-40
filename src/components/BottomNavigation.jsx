7b1eb477144cec4e5dd8c09854203268adb091fe
Feri Irawan <Feriirawans1998@gmail.com>
2021-11-23 16:50:48 +0700

    fix: Memperbaiki tampilan BottomNavigation

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -12,7 +12,7 @@ function BottomNavigation() {
     if (active) active = active.url
 
     return (
-        <div className="bottom-navigation fixed bottom-0 left-0 right-0 bg-yellow-600 dark:bg-gray-800 rounded-t-lg sm:rounded-t-none overflow-x-auto">
+        <div className="bottom-navigation fixed bottom-0 left-0 right-0 bg-yellow-600 dark:bg-gray-800 overflow-x-auto">
             <div className="w-full max-w-lg mx-auto bg-yellow-700 dark:bg-gray-900 dark:bg-opacity-20">
                 <div className="grid grid-cols-5 justify-center shadow">
                     {navs.map((nav, i) => (
@@ -20,8 +20,8 @@ function BottomNavigation() {
                             <a
                                 className={
                                     active == nav.url
-                                        ? `block text-center text-gray-100 px-3 md:px-5 pt-2 pb-1 duration-300 bg-yellow-800 dark:bg-gray-900`
-                                        : `block text-center text-gray-100 px-3 md:px-5 pt-2 pb-1 duration-300 bg-yellow-700 hover:bg-yellow-800 dark:bg-gray-900 dark:bg-opacity-20 dark:hover:bg-gray-900`
+                                        ? `block text-center text-gray-100 pt-2 pb-1 duration-300 bg-yellow-800 dark:bg-gray-900`
+                                        : `block text-center text-gray-100 pt-2 pb-1 duration-300 bg-yellow-700 hover:bg-yellow-800 dark:bg-gray-900 dark:bg-opacity-20 dark:hover:bg-gray-900`
                                 }
                             >
                                 <ion-icon
@@ -30,10 +30,9 @@ function BottomNavigation() {
                                             ? nav.icon.replace('-outline', '')
                                             : nav.icon
                                     }
+                                    style={{fontSize: '24px'}}
                                 ></ion-icon>
-                                <div className="text-xs sm:text-base">
-                                    {nav.name}
-                                </div>
+                                <div className="text-xs">{nav.name}</div>
                             </a>
                         </Link>
                     ))}
